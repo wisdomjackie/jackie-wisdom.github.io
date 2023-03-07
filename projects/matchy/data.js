0b14dd65820a4bbfdc3c7e391f355bdf7b309394
Jacqueline Wisdom <wisdomjackie@gmail.com>
2023-03-07 16:40:40 +0000

    description of commit

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -32,22 +32,42 @@ console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+//create a variable named noises and assign it to an empty array
+var noises = [];
+//use bracket notation to give noises it's first element: a string representing a noise your animal might make
+noises[0] = 'meow'; 
+//using an array function, add another noise to the END of noises
+noises.push('ekekek');
+//add an element to the beginning of noises using an array function
+noises.unshift('purr');
+//using bracket syntax again, add an element to the end of noises and do not hard code for the end
+noises[noises.length] = 'hiss';
+//console log the length of noises
+console.log(noises.length); 
+//console log the last element in noises without hard coding the index
+console.log(noises[noises.length - 1]);
+//console log the whole array
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//use bracket syntax to assign the noises property on animal to our new noises array
+animal['noises'] = noises; 
+//using any syntax add another noise to the noises property on animal 
+animal.noises.push('growl'); 
+//console log animal
+console.log(animal); 
 
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ * You can access properties on objects using either bracket notation or dot notation. In order to use dot notation on
+ * objects, you must know the exact name of the property/key. Otherwise, you must use bracket notation.
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * You can access elements on arrays using bracket notation only.
  * *******************************************************************
  */
 
@@ -63,13 +83,54 @@ console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//create a variable named animals and assign it to an empty array
+var animals = [];
+//push animal into animals
+animals.push(animal); 
+//console log animals
+console.log(animals); 
+//it looks like an array containing one object
+//create a variable called duck and assign it to the given data
+var duck = {
+  species: 'duck',
+  name: 'Jerome', 
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+}; 
+//push duck variable into animals array
+animals.push(duck); 
+//console log animals
+console.log(animals); 
+//it looks like an array containing two objects
+//create two more animal objects each with a species, a name, and at least two sounds sounds and add them to animals
+//create animal object dog named mal with sounds bark and woof
+var dog = {
+  species: 'dog', 
+  name: 'Mal', 
+  noises: ['bark', 'woof']
+};
 
+//create animal object momo, winged lemur who chirps and chitters
+var wingedLemur = {
+  species: 'winged lemur', 
+  name: 'Momo', 
+  noises: ['chirp', 'chitter']
+}
+//add each of these to animals
+animals.push(dog); 
+animals.push(wingedLemur); 
+//console log animals and console log the length of animals
+console.log(animals); 
+console.log(animals.length);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//choose a data structure for this list of friends and comment why
+//i choose an array to hold a list of friends because it is a collection of like elements
+//create a variable called friends and assign to chosen data structure
+var friends = [];
+//write a function called getRandom, that takes our animals array and returns a random index of input array, using math.random
 
 
 /**
