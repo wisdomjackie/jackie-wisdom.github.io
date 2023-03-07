364fc36aa6da924cb3f8bb663b85e061005502b0
Jacqueline Wisdom <wisdomjackie@gmail.com>
2023-03-07 15:22:37 +0000

    description of commit

--- Studies/variables.js
+++ Studies/variables.js
@@ -72,8 +72,94 @@ reference error. This is because the computer has not yet received this informat
 been declared at all. 
 
 These variables also differ in the ways that they are scoped. All three types, var, let and const, are scoped to functions. 
-This means that, if they are declared within a function 
+This means that, if they are declared within a function, then they only exist and can be used within that function. 
+
+    function printSomething() {//declare a function called printSomething
+        var number = 6;//declare a number variable and set to 6
+        console.log(number)//print the number variable to the console
+    }; 
+
+    printSomething(); //=> 6
+
+    console.log(number);//=> ReferenceError: number is not defined
+
+In the above example, the number variable is declared inside of the printSomething function block. When printSomething is
+invoked, number can be accessed. However, trying to reference number outside of the function will return an error message.
+This is because the var keyword creates variables that are scoped to functions. 
+
+Indeed, all three of the keywords var, let and const create variables that are scoped to functions. The above example
+will yield the same results if let or const is used instead of var.
+
+    function printSomething() {//declare a function called printSomething
+        const number = 6;//declare a number variable and set to 6
+        console.log(number)//print the number variable to the console
+    }; 
+
+    printSomething(); //=> 6
+
+    console.log(number);//=> ReferenceError: number is not defined
+
+Just as variables can be scoped to function blocks, so they can be scoped to if blocks and loop blocks. Here is where 
+another difference exists between these types of variables. Var keyword variables are not scoped to if blocks and they
+are not scoped to loop blocks. 
+
+    for (var i = 0; i < 5; i++) {//create a for loop starting at index 0, stopping at index 4 and incrementing by 1
+        var orbits = i //declare orbits variable and set to i
+    } 
+  
+    console.log(orbits)//=> 4
+
+In the above example, orbits can be referenced outside of the for loop in which it was created. This is because var keyword
+variables are not loop block scoped. This is not the case for let and const. 
+
+    for (var i = 0; i < 5; i++) {//create a for loop starting at index 0, stopping at index 4 and incrementing by 1
+        let orbits = i //declare orbits variable and set to i
+    } 
+  
+    console.log(orbits)//=> ReferenceError: orbits is not defined
+    
+The above code will return a reference error if either let or const are used to declare orbits. In such a case, orbits 
+would only be able to be used or referenced inside of the loop in which they were declared. This is because let and const
+are both scoped to loop blocks. 
+
+Just as variables can be scoped to function or loop blocks, they can also be scoped to if blocks. Var keyword variables 
+are not scoped to if blocks. 
+
+    if (2 > 1) {//if the condition of 2 > 1
+        var result = true;// declare result variable and set to value of true
+    }
+  
+    console.log(result)//=> true
+
+In the above example, result is decalred inside of an if block. If logged to the console outside of the if block, it will
+be recognized as a variable and return the value it was assigned to. However, if let or const were used to declare results
+this would not be the case.
+
+    if (2 > 1) {// if the condition of 2 > 1
+        const result = true;//create variable result and assign to true
+    }
+  
+    console.log(result)//=> ReferenceError: result is not defined
+
+Above, attempting to use the result variable outside of the if block will return a reference error. This is because both
+let and const are indeed scoped to if blocks. They cannot be used or referenced outside of an if block, if they were
+declared inside of one.
 
 
+To sum up the differences between these three types of variables:
 
-
+    var: reassignable
+         hoisted
+         scoped to functions
+         NOT scoped to loop blocks
+         NOT scoped to if blocks
+    let: reassignable
+         NOT hoisted
+         scoped to functions
+         scoped to loop blocks
+         scoped to if blocks
+    const: NOT reassignable
+           NOT hoisted
+           scoped to functions
+           scoped to loop blocks
+           scoped to if blocks
