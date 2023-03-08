9e3c20d4b58031e8a224c8f173ec4ee8508122ca
Jacqueline Wisdom <wisdomjackie@gmail.com>
2023-03-08 20:57:45 +0000

    description of commit

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -35,18 +35,55 @@ console.log(search(animals, 'Landon'),9)
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//declare a function replace that takes 3 parameters: an array of animals, string for animal name, object for replacement
+function replace(array, string, object) {
+    //if an animal with that name exists in the array, replace it with the object
+    //for loop to access each animal in the array
+    for (i = 0; i < array.length; i++){
+        //determine if the name value exists in array[i]
+        if (array[i].name === string) {
+            //now replace whole object with new object
+            //OMG use splice. sick
+            array.splice(array[i], 1, object)
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//declare function remove which takes 2 parameters: array of animals and name of animal
+function remove(array, string) {
+    //access animals in the array
+    for (var i = 0; i < array.length; i++) {
+        //determine if animal with that name exists
+        if (array[i].name === string) {
+            //then remove from array
+            array.splice(array[i],1)
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//declare function add with 2 parameters: array of animals and object to be added
+function add(array, object) {
+    //first, determine if array already has input object. use for loop
+    for (var i = 0; i <array.length; i++) {
+        //if array already containts input object by it's name
+        if (array[i].name === object.name) {
+            //then return null
+            return null
+        }
+    }
+        //else if name length is greater than zero and species length is greater than 0
+    if (object.name.length > 0 && object.species.length > 0) {
+        //then add the object to array
+        array.push(object)
+    } 
+}
 
 
 /**
