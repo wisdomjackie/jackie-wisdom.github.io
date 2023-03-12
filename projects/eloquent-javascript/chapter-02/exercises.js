8027c2666ebc0f147b6bffa8435e08e17fccd84f
Jacqueline Wisdom <wisdomjackie@gmail.com>
2023-03-12 00:51:10 +0000

    description of commit

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -18,6 +18,16 @@ for (let i = 0; i < rows; i++) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
+  for (let i = 1; i < 16; i++){
+    //if divisible by three print Fizz
+    if (i % 15 === 0) {
+      console.log('fizzbuzz')
+    }else if (i % 5 === 0) {
+      console.log('buzz') 
+    }else if (i % 3 === 0) {
+      console.log('fizz')
+    } else console.log(i)
+  }
   
 }
 
