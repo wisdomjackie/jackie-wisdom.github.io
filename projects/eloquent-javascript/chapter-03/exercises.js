2865480070991c9592ede0ab066e856c76f78979
Jacqueline Wisdom <wisdomjackie@gmail.com>
2023-03-12 02:55:21 +0000

    description of commit

--- projects/eloquent-javascript/chapter-03/exercises.js
+++ projects/eloquent-javascript/chapter-03/exercises.js
@@ -10,7 +10,14 @@ return Math.min(a,b)
 // isEven //////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function isEven() {
+function isEven(num) {
+  if (num === 0){
+    return true
+  } else if (num === 1) {
+    return false
+  } else if ((num - 2) % 2 === 0){
+    return true
+  } else return false
 
 }
 
